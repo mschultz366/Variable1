a02e098dd53057153f45c974c993da1707102d29
unknown <mschultz366@students.mchenry.edu>
2019-02-06 19:49:57 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*byte
+         * short
+         * int
+         * long
+         * float
+         * double
+         * boolean
+         * char*/
+
+        System.out.println("Byte size: " + Byte.SIZE);
+        System.out.println("Short size: " + Short.SIZE);
+        System.out.println("Int size: " + Integer.SIZE);
+        System.out.println("Long size: " + Long.SIZE);
+        System.out.println("Float size: " + Float.SIZE);
+        System.out.println("Double size: " + Double.SIZE);
+        System.out.println("Boolean type: " + Boolean.TYPE);
+        System.out.println("Char size: " + Character.SIZE);
+
+    }
+}
